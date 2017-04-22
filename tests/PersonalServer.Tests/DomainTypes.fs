5a1f77e212bc0f8d09eb8968b443ebaf7358d1bd
Jack Fox <jackfoxy@gmail.com>
2017-04-22 10:22:08 -0700

    fix replay seed config

--- tests/PersonalServer.Tests/DomainTypes.fs
+++ tests/PersonalServer.Tests/DomainTypes.fs
@@ -7,7 +7,7 @@ open FsCheck
 module DomainTypes =
 
     let config10k = { FsCheckConfig.defaultConfig with maxTest = 10000 }
-    let configReplay = { FsCheckConfig.defaultConfig with replay = Some <| (1919174669,296290663) }  //see Tips & Tricks for FsCheck
+    let configReplay = { FsCheckConfig.defaultConfig with maxTest = 10000; replay = Some <| (1332099450, 296293084) }  //see Tips & Tricks for FsCheck
  
     Arb.register<DomainGenerators>() |> ignore
 
@@ -15,20 +15,20 @@ module DomainTypes =
     let testFullName =
         testList "DomainTypes.FullName" [
 
-            testPropertyWithConfig config10k "test label" <|
-                fun  () ->
-                    Prop.forAll (DomainGenerators.FullName()) 
-                        (fun (fullName : FullName) -> 
-                                
-                                let first = fullName.First |> Option.map (fun x -> x.Value)
-                                let middle = fullName.Middle |> List.map (fun x -> x.Value)
-                                let family = fullName.Family |> Option.map (fun x -> x.Value)
-                                //note: custom label does not print on failure
-                                1 = 2|@ sprintf "first: %A middle: %A family: %A"  fullName.First middle fullName.Family
-                            )
+//            testPropertyWithConfig config10k "test label" <|
+//                fun  () ->
+//                    Prop.forAll (DomainGenerators.FullName()) 
+//                        (fun (fullName : FullName) -> 
+//                                
+//                                let first = fullName.First |> Option.map (fun x -> x.Value)
+//                                let middle = fullName.Middle |> List.map (fun x -> x.Value)
+//                                let family = fullName.Family |> Option.map (fun x -> x.Value)
+//                                //note: custom label does not print on failure
+//                                1 = 2|@ sprintf "first: %A middle: %A family: %A"  fullName.First middle fullName.Family
+//                            )
 
-            testPropertyWithConfig config10k "this will fail with config10k, then succeed with configReplay" <|
-//            testPropertyWithConfig configReplay "this will fail with config10k, then succeed with configReplay" <|
+//            testPropertyWithConfig config10k "this will fail with config10k, then succeed with configReplay" <|
+            testPropertyWithConfig configReplay "this will fail with config10k, then succeed with configReplay" <|
                 fun  () ->
                     Prop.forAll (DomainGenerators.FullName()) 
                         (fun (fullName : FullName) -> 
@@ -43,16 +43,16 @@ module DomainTypes =
                                 t.Value = fullName
                             )
 
-            testPropertyWithConfig config10k "This should have registered FullName above" <|
-                fun  (fullName : FullName) ->
-
-                    let t = 
-                        let first = fullName.First |> Option.map (fun x -> x.Value)
-                        let middle = fullName.Middle |> List.map (fun x -> x.ToString())
-                        let family = fullName.Family |> Option.map (fun x -> x.Value)
-                        FullName.TryParse (first, middle, family,  NameOrder.Western, Set.empty<Tag>)
- 
-                    t.Value = fullName
+//            testPropertyWithConfig config10k "This should have registered FullName above" <|
+//                fun  (fullName : FullName) ->
+//
+//                    let t = 
+//                        let first = fullName.First |> Option.map (fun x -> x.Value)
+//                        let middle = fullName.Middle |> List.map (fun x -> x.ToString())
+//                        let family = fullName.Family |> Option.map (fun x -> x.Value)
+//                        FullName.TryParse (first, middle, family,  NameOrder.Western, Set.empty<Tag>)
+// 
+//                    t.Value = fullName
             ]
 
     
