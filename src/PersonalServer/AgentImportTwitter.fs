a7b87c5ddb92b676fa4892864a382637f1e6d2e1
Jack Fox <jackfoxy@gmail.com>
2017-06-03 09:59:16 -0700

    get twitter followers

---
+++ src/PersonalServer/AgentImportTwitter.fs
@@ -0,0 +1,24 @@
+﻿namespace Jackfoxy.PersonalServer
+
+open Tweetinvi
+
+module AgentImportTwitter =
+
+    type TwitterCredentials =
+        {
+        ConsumerKey : string
+        ConsumerSecret : string
+        UserAccessToken : string
+        UserAccessSecret : string
+        }
+
+    let getFollowers twitterCredential = 
+        Auth.SetUserCredentials(twitterCredential.ConsumerKey, twitterCredential.ConsumerSecret, twitterCredential.UserAccessToken, twitterCredential.UserAccessSecret)
+        |> ignore
+        let user = User.GetAuthenticatedUser()
+
+        user.GetFollowers(5000)
+        |> Seq.cast<Models.IUser>
+        |> Seq.toList
+
+
